4c4dfb1525b547ae7bde6518b73dd4083e34e765
Ni30kr91 <nitish.kumar8092@gmail.com>
2022-06-08 16:26:50 +0530

    division of 2

---
+++ div.go
@@ -0,0 +1,12 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 1225
+	num2 := 35
+	result := num1 / num2
+	fmt.Println("result is: ", result)
+}
